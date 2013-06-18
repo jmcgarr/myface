7093bb0d3e3006712332d85a42b3d1e7738aa567
Mike McGarr <jmcgarr@gmail.com>
2013-06-18 15:26:48 -0400

    Updating the version of the cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'it@somewhere.com'
 license          'All rights reserved'
 description      'Installs/Configures jmcgarr-myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
